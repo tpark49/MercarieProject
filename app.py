e7f1265195c0536df95d305ea5aa88659df87d6d
tpark49 <typ5120@gmail.com>
2021-11-25 19:51:38 -0500

    working on app

--- app.py
+++ app.py
@@ -28,6 +28,8 @@ engine = create_engine(f"postgresql+psycopg2://{config.user_id}:{config.password
 
 #query for certain categories in category_1 then return df 
 def query_category(*args):
+    print(args)
+
     if len(args)==1: 
         category_1 = args[0]
         category_1_query = "and category_1 = '"+category_1+"' "
@@ -70,7 +72,7 @@ category_options = pd.read_sql_query(
     "where category_1 is not null "\
     "limit 13", engine)
 
-# # #initialize second category list before updating
+##initialize second category list before updating
 category_options_2 = pd.read_sql_query(
     "select category_2 from (select category_2, count(*) as count_cat "\
     "from mercariedata1 where category_2 is not null "\
@@ -78,15 +80,15 @@ category_options_2 = pd.read_sql_query(
     "order by count_cat desc) as t "\
     "limit 10", engine)
 
-# #query unique categories in category_2 
-# category_options_3 = pd.read_sql_query(
-#     "select category_3 from (select category_3, count(*) as count_cat "\
-#     "from mercariedata1 "\
-#     "group by category_2 "\
-#     "order by count_cat desc "\
-#     "limit 100) as t "\
-#     "where category_2 is not null "\
-#     "limit 13", engine)
+##query unique categories in category_3 
+category_options_3 = pd.read_sql_query(
+    "select category_3 from (select category_3, count(*) as count_cat "\
+    "from mercariedata1 "\
+    "group by category_3 "\
+    "order by count_cat desc "\
+    "limit 100) as t "\
+    "where category_3 is not null "\
+    "limit 13", engine)
 
 
 #create dictionary for category_1
@@ -114,7 +116,7 @@ first_card = dbc.Card(
                 dbc.DropdownMenuItem(item, id=f"dd_{item}") for item in category_options["category_1"]
             ]), 
         html.Br(),
-        html.H5("Pick First Subcategory"), 
+        html.H5("First Subcategory"), 
         dcc.Dropdown(
             id = "second_dropdown",
             options = [
@@ -122,8 +124,15 @@ first_card = dbc.Card(
             ], 
             value="Pick your Second Category" 
         ),
-
-
+        html.Br(),
+        html.H5("Second Subcategory"), 
+        dcc.Dropdown(
+            id = "third_dropdown",
+            options = [
+                {"label":x, "value":x} for x in category_options_3["category_3"]
+            ], 
+            value="Pick your Third Category" 
+        ),
         html.Br(),  
         html.Div(
             [dbc.Button("Search!",id="search_button", outline=True, color="primary", size="lg")],
@@ -153,18 +162,45 @@ app.layout = dbc.Container(
                     id="loading_graph",
                     type="default",
                     children=[
-                        dcc.Graph(id="distplot")
+                        dcc.Graph(id="distplot"),
+                        dcc.Graph(id="boxplot")
                     ]
                 )
                 )
         ]
-    ) 
+    )
 ], 
 fluid=True
 )
 
 
 ###callback functions
+@app.callback(
+    Output("third_dropdown", "options"),
+    [
+        Input("first_dropdown", "label"),
+        Input("second_dropdown", "value") 
+    ]
+)
+def change_third_dropdown(cat_1_label, cat_2_label):
+    if cat_2_label == None or cat_1_label == None:
+        raise PreventUpdate
+
+    category_3_options = pd.read_sql_query(
+        "select category_3 from "\
+        "(select category_3, count(*) as count_cat_3 "\
+        "from mercariedata1 "\
+        "where category_1 = '" +cat_1_label+"' and category_2= '" + cat_2_label + "' "\
+        "group by category_3 "\
+        "order by count_cat_3 desc) as t "\
+        "limit 10", engine)
+
+    options = [
+        {"label":x, "value":x} for x in category_3_options["category_3"]
+        ]
+
+    return options
+
 
 @app.callback(
     Output("second_dropdown", "options"),
@@ -190,25 +226,55 @@ def change_second_dropdown(cat_1_label):
     return options
 
 
+#callback for distplot
 @app.callback(
     Output("distplot", "figure"),
     [Input("search_button","n_clicks" ), 
     State(component_id="first_dropdown", component_property="label"), 
-    State(component_id="second_dropdown", component_property="value")
+    State(component_id="second_dropdown", component_property="value"),
+    State(component_id="third_dropdown", component_property="value")
     ]
 )
-def update_graph(button_click, category_label, second_label):
-    print(category_label)
-    if category_label == "Category" or category_label == None:
+def update_graph(button_click, category_label, second_label, third_label):
+    if category_label == "Category":
         raise PreventUpdate
-    cat_df = query_category(category_label)
-    print(cat_df)
-    prices = [cat_df[np.abs(stats.zscore(cat_df["price"]))<2]["price"]]
+    cat_df = query_category(category_label, second_label, third_label)
+
+    prices = [cat_df[np.abs(stats.zscore(cat_df["price"]))<1.65]["price"]]
     group_labels = ["distplot"]
     fig = ff.create_distplot(
         prices,
         group_labels,
         curve_type = "kde"
+    )
+    # fig.add_vline(
+    #     x = np.mean(cat_df.price), 
+    #     line_dash ="dash", 
+    #     line_color = "blue"
+    # )
+    return fig
+
+
+#callback for boxplot
+@app.callback(
+    Output("boxplot", "figure"),
+    [Input("search_button","n_clicks" ), 
+    State(component_id="first_dropdown", component_property="label"), 
+    State(component_id="second_dropdown", component_property="value"),
+    State(component_id="third_dropdown", component_property="value")
+    ]
+)
+def update_graph(button_click, category_label, second_label, third_label):
+    if category_label == "Category":
+        raise PreventUpdate
+    cat_df = query_category(category_label, second_label, third_label)
+
+    prices = [cat_df[np.abs(stats.zscore(cat_df["price"]))<1.65]["price"]]
+    group_labels = ["distplot"]
+    fig = px.box(
+        cat_df,
+        x="price", 
+        title = "Pricing Distribution"
     )
     # fig.add_vline(
     #     x = np.mean(cat_df.price), 
@@ -235,6 +301,9 @@ def update_label(*input_list):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True,host = '127.0.0.1')
+    app.run_server(debug=True,
+                    host = '127.0.0.1',
+                    port = 8051
+                    )
 
 
